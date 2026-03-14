9104c992501e59773182510224f54ef9f6898e22
Iñaki Calvo <inakinaiz@gmail.com>
2026-03-14 14:27:23 +0100

    Use native local font function

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import icon from 'astro-icon';
 
 // https://astro.build/config
@@ -12,6 +12,41 @@ export default defineConfig({
     responsiveStyles: true,
     layout: 'constrained',
   	},
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: 'Atkinson Hyperlegible Next',
+			cssVariable: '--font-atkinson',
+			options: {
+				variants: [
+					{
+						src: ['./src/assets/fonts/AtkinsonHyperlegibleNext-VariableFont_wght.ttf'],
+						weight: '100 900',
+						style: 'normal',
+					},
+					{
+						src: ['./src/assets/fonts/AtkinsonHyperlegibleNext-Italic-VariableFont_wght.ttf'],
+						weight: '100 900',
+						style: 'italic',
+					},
+				],
+			},
+		},
+		{
+			provider: fontProviders.local(),
+			name: 'Atkinson Hyperlegible Mono',
+			cssVariable: '--font-atkinson-mono',
+			options: {
+				variants: [
+					{
+						src: ['./src/assets/fonts/AtkinsonHyperlegibleMono-VariableFont_wght.ttf'],
+						weight: '100 900',
+						style: 'normal',
+					},
+				],
+			},
+		},
+	],
 	integrations: [
 		mdx(),
 		sitemap(),
